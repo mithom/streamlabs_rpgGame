581a97746321170382bbaa8251e56343bc500cd9
Thomas Michiels <mi_thom@hotmail.com>
2018-09-09 22:51:42 +0200

    delete unused timezones + feedback on special cooldown

--- lib/characters.py
+++ lib/characters.py
@@ -199,6 +199,12 @@ class Character(object):
             special = special_list[0]
             if special.unavailable_until is None or special.unavailable_until < dt.datetime.now(utc):
                 special.use(target)
+            else:
+                Parent.SendStreamMessage(self.format_message(
+                    "{0}, you can not use that special right now, cooldown: {1}",
+                    self.Parent.GetDisplayName(self.user_id),
+                    str(special.unavailable_until - dt.datetime.now(utc))
+                ))
         else:
             self.Parent.SendStreamMessage(self.format_message(
                 "{0}, your character {1} doesn't have that special",
